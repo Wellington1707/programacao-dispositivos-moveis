f6108b7b4ee74d75f3de7fc7abbf6911b209b0bd
wellingtonmfs@hotmail.com <wellingtonmfs@hotmail.com>
2025-03-06 20:53:16 -0300

    Adicionando arquivos da pratica02

---
+++ praticas/pratica02/App.js
@@ -0,0 +1,31 @@
+import { StyleSheet, Text, View } from "react-native";
+function App() {
+  return (
+    <View style={{ flex: 1 }}>
+  
+      <AppBar titulo="Minha Aplicação React Native" />
+
+      <View style={styles.container}>
+        <Text style={{fontSize: 20, fontWeight: "bold", color: "#fff"}}>Minha Aplicação React Native</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {flex: 1, backgroundColor: "#fff", justifyContent: "center", alignItems: "center"},
+  appBar: {height: 50, backgroundColor: "#333", justifyContent: "center", paddingLeft: 16},
+  title: {fontSize: 20, fontWeight: "bold", color: "#fff"},
+});
+
+
+
+function AppBar(props) {
+  return (
+    <View style={styles.appBar}>
+      <Text style={styles.title}>{props.titulo}</Text>
+    </View>
+  );
+}
+
+export default App;
