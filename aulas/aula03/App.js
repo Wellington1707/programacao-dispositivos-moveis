0bbac43c222a63c9c2196674d7e04f896ac9ee06
wellingtonmfs@hotmail.com <wellingtonmfs@hotmail.com>
2025-03-11 21:56:41 -0300

    codigos da aula03

--- aulas/aula03/App.js
+++ aulas/aula03/App.js
@@ -1,27 +1,12 @@
-import { View } from "react-native";
+import Flexbox from "./Flexbox";
+import PadraoAndroid from "./PadraoAndroid";
+import PadraoIOS from "./PadraoIOS";
+import GaleriaScreen from "./GaleriaScreen";
 
 function App() {
   return (
-    <View 
-      style={{ flex: 1, flexDirection: "row", backgroundColor: "white" }}>
-      <View style={{
-         flex: 1,
-         justifyContent: "space-evenly", 
-         alignItems:"stretch", 
-         backgroundColor: "red" 
-         }}
-         >
-        <View
-          style={{ height: 50,  backgroundColor: "black" }}
-        ></View>
-        <View style={{ height: 50,  backgroundColor: "blue" }}></View>
-        <View
-          style={{ height: 50,  backgroundColor: "yellow" }}
-        ></View>
-      </View>
-      <View style={{ flex: 1, backgroundColor: "green" }}></View>
-    </View>
-  );
+    <GaleriaScreen/>
+    )
 }
 
 export default App;
