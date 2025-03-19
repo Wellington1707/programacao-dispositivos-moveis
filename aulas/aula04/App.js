34c6991faa570a080a82b1bd816242848fd9147c
wellingtonmfs@hotmail.com <wellingtonmfs@hotmail.com>
2025-03-18 21:46:03 -0300

    adicionando arquivos da aula04

---
+++ aulas/aula04/App.js
@@ -0,0 +1,10 @@
+import GaleriaFotos from "./screens/GaleriaFotos";
+import RegistrarConta from "./screens/RegistrarContas";
+
+function App(){
+  return (
+    <GaleriaFotos/>
+  );
+};
+
+export default App;
